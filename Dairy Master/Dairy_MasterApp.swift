68abe9defe06e0813defb7edb6ed3dc5bbe2d520
Evgenij <evgenij.voronov@icloud.com>
2022-08-23 17:42:07 +0300

    Initial Commit

---
+++ Dairy Master/Dairy_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dairy_MasterApp.swift
+//  Dairy Master
+//
+//  Created by Евгений Воронов on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dairy_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
